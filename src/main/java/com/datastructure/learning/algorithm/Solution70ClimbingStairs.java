cac50532e41ce59c4a6fe281bb67c0c127d48a41
awutang <779706234@qq.com>
2024-01-22 20:50:21 +0800

    climb stairs dp

---
+++ src/main/java/com/datastructure/learning/algorithm/Solution70ClimbingStairs.java
@@ -0,0 +1,75 @@
+package com.datastructure.learning.algorithm;
+
+/**
+ * You are climbing a staircase. It takes n steps to reach the top.
+ *
+ * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: n = 2
+ * Output: 2
+ * Explanation: There are two ways to climb to the top.
+ * 1. 1 step + 1 step
+ * 2. 2 steps
+ * Example 2:
+ *
+ * Input: n = 3
+ * Output: 3
+ * Explanation: There are three ways to climb to the top.
+ * 1. 1 step + 1 step + 1 step
+ * 2. 1 step + 2 steps
+ * 3. 2 steps + 1 step
+ *
+ *
+ * Constraints:
+ *
+ * 1 <= n <= 45
+ */
+public class Solution70ClimbingStairs {
+    /**
+     * 爬到第一层楼梯有一种方法，爬到二层楼梯有两种方法。
+     * 那么第一层楼梯再跨两步就到第三层 ，第二层楼梯再跨一步就到第三层。
+     * 所以到第三层楼梯的状态可以由第二层楼梯 和 到第一层楼梯状态推导出来，那么就可以想到动态规划了。
+     */
+    // 动态规划五部曲
+    // 1.状态定义：dp[i]为爬到第i层楼梯的方法数
+    // 2.状态转移方程：dp[i]=dp[i-1]+dp[i-2] 因为到第i层可以从i-1爬上来，也可以从i-2爬两层
+    // 3.初始化dp[1]=1 dp[2]=2
+    // 4.确定遍历顺序，从前往后
+    // 5.日志打印排查
+    // 发现2中的状态定义转移方程就是斐波拉契数列
+    public static int climbStairs(int n) {
+
+        if (n ==1) return 1;
+        // 1.状态定义--但是这里可以优化下空间，因为是递推
+        // int[] dp = new int[n + 1];
+        // int[] dp = new int[3];
+
+        int[] dp = new int[3];
+
+        // 3.
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <=n; i++) {
+            // 2+4 从前往后遍历
+            int sum = dp[1] + dp[2];
+            dp[1] = dp[2];
+            dp[2] = sum;
+        }
+        return dp[2];
+
+    }
+
+    public static void main(String[] args) {
+        climbStairs(5);
+
+    }
+
+
+
+
+}
