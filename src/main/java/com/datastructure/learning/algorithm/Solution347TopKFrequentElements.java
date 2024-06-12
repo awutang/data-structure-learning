441985aa79c40fce838c75109d954e942e8aabf0
awutang <779706234@qq.com>
2024-06-12 16:27:42 +0800

    top frequent

---
+++ src/main/java/com/datastructure/learning/algorithm/Solution347TopKFrequentElements.java
@@ -0,0 +1,24 @@
+package com.datastructure.learning.algorithm;
+
+/**
+ * Given an integer array nums and an integer k, return the k most frequent elements.
+ * You may return the answer in any order.
+ *
+ *
+ *
+ * Example 1:
+ *
+ * Input: nums = [1,1,1,2,2,3], k = 2
+ * Output: [1,2]
+ * Example 2:
+ *
+ * Input: nums = [1], k = 1
+ * Output: [1]
+ */
+public class Solution347TopKFrequentElements {
+
+
+    public int[] topKFrequent(int[] nums, int k) {
+
+    }
+}
